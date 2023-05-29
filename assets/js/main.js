1703ef56f842d2aebb04ebd460be7b1352f85716
freetimezp <kraftukrnet@ukr.net>
2023-05-29 17:37:49 +0300

    06. js-eatme-09-sushi. Animation AOS.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,14 @@ import sushi12 from '../images/sushi-12.png';
 import sushi11 from '../images/sushi-11.png';
 import sushi10 from '../images/sushi-10.png';
 
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-// // init AOS animation
-// AOS.init({
-//     duration: 1000,
-//     offset: 100,
-// });
+//init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
 
 const trendingSushis = [
     'Make Sushi',
